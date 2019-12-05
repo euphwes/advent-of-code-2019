a0980f4eaa83a054d368b16ae24d05d62246c35c
Wes Evans <euphwes@gmail.com>
2019-12-05 10:46:33 -0700

    Added a get_tokenized_input utility function to retrieve the day's input as lists of (optionally transformed) tokens.

--- aoc_util/input.py
+++ aoc_util/input.py
@@ -4,6 +4,9 @@ from . import __get_code_file_no_ext
 
 INPUT_FILENAME_TEMPLATE = 'inputs/input_{day_num}.txt'
 
+# simple placeholder lambda which does nothing
+DO_NOTHING = lambda token: token
+
 #------------------------------------------------------------------------------
 
 def get_input():
@@ -14,6 +17,23 @@ def get_input():
     input_lines = [x.replace('\n', '') for x in open(input_file).readlines()]
 
     return input_lines
+
+
+def get_tokenized_input(split_str, transform=DO_NOTHING):
+    """ Returns the input for the current AoC day, where each line is split
+    by the supplied string and collected into a list of tokens, and the
+    entire input is returned as a list of token lists. Optionally, the caller
+    can supply a function to transform each token into a desired format.
+
+    Ex.
+    1,2,3           [['1', '2', '3'],
+    4,5,6  ------>   ['4', '5', '6'],
+    7,8,9            ['7', '8', '9']] """
+
+    tokenized   = [line.split(split_str) for line in get_input()]
+    transformed = [[transform(t) for t in line] for line in tokenized]
+
+    return transformed
 
 
 def __get_input_filename():
