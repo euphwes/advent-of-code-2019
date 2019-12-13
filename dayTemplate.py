826afbdb90183a413d3d5e8ac758bd57e2c171b0
Wes Evans <euphwes@gmail.com>
2019-12-13 15:45:03 -0700

    Added a template solution file, to make creating new files for each day just a smidge easier.

---
+++ dayTemplate.py
@@ -0,0 +1,26 @@
+from aoc_util.input import get_input
+from aoc_util.decorators import aoc_output_formatter
+
+# -----------------------------------------------------------------------------
+
+# helper functions, constants, etc here
+
+# -----------------------------------------------------------------------------
+
+@aoc_output_formatter(2019, 0, 1, 'custom output label')
+def part_one(orbital_name_pairs):
+    pass
+
+
+@aoc_output_formatter(2019, 0, 2, 'custom output label')
+def part_two(orbital_name_pairs):
+    pass
+
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    problem_input = get_input()
+
+    part_one(problem_input)
+    part_two(problem_input)
